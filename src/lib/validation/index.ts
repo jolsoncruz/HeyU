e2d4cd4905fcdf055682813c3565ab6d79c23977
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2024-01-12 01:52:45 -0500

    Initial Commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,8 @@
+import * as z from "zod"
+
+export const SignUpValidation = z.object({
+    name: z.string().min(2, { message: 'Name must be atleast 2 characters'}),
+    username: z.string().min(2, {message: 'Username must be atleast 2 characters'}),
+    email: z.string().email(),
+    password: z.string().min(8, {message: 'Password must be atleast 8 characters'})
+})
