adc63525600925c2d4ee87d5d682c62e6024cfeb
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2024-01-25 08:10:33 -0500

    CreatePost form

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -11,3 +11,10 @@ export const SignInValidation = z.object({
     email: z.string().email(),
     password: z.string().min(8, {message: 'Password must be atleast 8 characters'})
 })
+
+export const PostValidation = z.object({
+    caption: z.string().min(5, { message: 'Caption must be atleast 5 characters'}).max(22000),
+    file: z.custom<File[]>(),
+    location: z.string().min(2, {message: 'Location must be atleast 2 characters'}).max(100),
+    tags: z.string()
+})
