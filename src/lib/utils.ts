d315c9134cd40d1c9f6420b7e1906bde63abc14e
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2024-02-09 21:11:17 -0500

    Implemented Post Cards and Stats

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,49 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDateString(dateString: string): string {
+  const date = new Date(dateString);
+  const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+      timeZone: 'UTC'
+  };
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+}
+
+export function formatRelativeTime(dateString: string): string {
+  const date = new Date(dateString);
+  const now = new Date();
+
+  const diffInMilliseconds = now.getTime() - date.getTime();
+  const diffInSeconds = Math.floor(diffInMilliseconds / 1000);
+  const diffInMinutes = Math.floor(diffInSeconds / 60);
+  const diffInHours = Math.floor(diffInMinutes / 60);
+  const diffInDays = Math.floor(diffInHours / 24);
+  const diffInWeeks = Math.floor(diffInDays / 7);
+  const diffInYears = Math.floor(diffInDays / 365);
+
+  if (diffInMinutes < 1) {
+      return 'Just now';
+  } else if (diffInMinutes < 60) {
+      return `${diffInMinutes} minute${diffInMinutes === 1 ? '' : 's'} ago`;
+  } else if (diffInHours < 24) {
+      return `${diffInHours} hour${diffInHours === 1 ? '' : 's'} ago`;
+  } else if (diffInDays < 7) {
+      return `${diffInDays} day${diffInDays === 1 ? '' : 's'} ago`;
+  } else if (diffInWeeks < 52) {
+      return `${diffInWeeks} week${diffInWeeks === 1 ? '' : 's'} ago`;
+  } else {
+      return `${diffInYears} year${diffInYears === 1 ? '' : 's'} ago`;
+  }
+}
+
+export const checkIsLiked = (likeList: string[], userId: string) => {
+  return likeList.includes(userId);
+};
